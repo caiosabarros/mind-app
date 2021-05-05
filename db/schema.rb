9ab5f4759410fb5c16f9c51a526cbfc9a122be0e
Caio Sa <caiohenriquesa1@gmail.com>
2021-05-04 22:00:59 -0300

    Creating the Tag model
    Task management: https://trello.com/b/9g38JFRL/mindapp
    
    Extra: adding the has_many relationships between the Content model and the
    User model and aslo in between the User model and the Tag model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_230824) do
+ActiveRecord::Schema.define(version: 2021_05_05_005417) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.citext "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,6 @@ ActiveRecord::Schema.define(version: 2021_05_04_230824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "users"
+  add_foreign_key "tags", "users"
 end
